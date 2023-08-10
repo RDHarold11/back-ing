8d5c280f6c251eabb5e8138c599059c1662f5b1c
Harold Manuel <aquinoharold460@gmail.com>
2023-08-09 21:55:10 -0400

    Agregar

--- routes/articuloRutas.js
+++ routes/articuloRutas.js
@@ -3,9 +3,9 @@ const router = Router();
 import controlador from "../controladores/controladorArticulo.js";
 import protect from "../middleware/authMiddleware.js";
 
-router.route("/").post(protect, controlador.postArticle);
+router.route("/").post(controlador.postArticle);
 
-router.route("deletepost/:id").delete(protect, controlador.deleteArticles);
+router.route("deletepost/:id").delete(controlador.deleteArticles);
 
 router.get("/getarticles", controlador.getArticles);
 
