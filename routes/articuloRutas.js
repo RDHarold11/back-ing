5808cc0bf9fa5a646646774b20bb5f5c42c308dc
Harold Manuel <aquinoharold460@gmail.com>
2023-08-15 15:12:37 -0400

    Ahora si

--- routes/articuloRutas.js
+++ routes/articuloRutas.js
@@ -9,14 +9,9 @@ router.route("deletepost/:id").delete(controlador.deleteArticles);
 
 router.get("/getarticles", controlador.getArticles);
 
-<<<<<<< HEAD
-router.get("/:id", controlador.getArticleById);
-=======
 router.get("/getArticlesBy/:type", controlador.getArticleByType);
 
-router.get('/getRecentsArticles', controlador.getRecentArticles);
-
->>>>>>> 2d1948779b8255707d3fa564c4407de17ac75c58
+router.get("/getRecentsArticles", controlador.getRecentArticles);
 
 router.patch("/:id", controlador.updateArticle);
 export default router;
